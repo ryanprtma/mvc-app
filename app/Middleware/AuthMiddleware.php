239ef13b3d0c921376647989865b7c5b9ceb1a7a
ryan apratama <ryanapratama16@gmail.com>
2025-03-04 21:58:34 +0700

    Add auth middleware to home route

---
+++ app/Middleware/AuthMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ryanprtma\MvcApp\Middleware;
+
+use Ryanprtma\MvcApp\Middleware\Middleware;
+
+class AuthMiddleware implements Middleware
+{
+
+    function before(): void
+    {
+        session_start();
+        if(!isset($_SESSION['user'])) {
+            header('Location: /login');
+            exit();
+        }
+    }
+}
