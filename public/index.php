1c6b69cec796260bfe04ce9082c9a27cee582ccc
ryan apratama <ryanapratama16@gmail.com>
2025-03-05 05:18:03 +0700

    Add more routes

--- public/index.php
+++ public/index.php
@@ -2,10 +2,20 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Ryanprtma\MvcApp\Controller\AuthController;
+use Ryanprtma\MvcApp\Controller\CharacterController;
 use Ryanprtma\MvcApp\Controller\HomeController;
-use Ryanprtma\MvcApp\Middleware\AuthMiddleware;
+use Ryanprtma\MvcApp\Controller\UserController;
 use Ryanprtma\MvcApp\Router;
 
-Router::add('GET', '/', HomeController::class, 'index', [AuthMiddleware::class]);
+Router::add('GET', '/', HomeController::class, 'index', []);
+Router::add('GET', '/users/register', UserController::class, 'register', []);
+Router::add('POST', '/users/register', UserController::class, 'signUp', []);
+Router::add('GET', '/users/login', AuthController::class, 'login', []);
+Router::add('POST', '/users/login', AuthController::class, 'authenticate', []);
+Router::add('GET', '/users/logout', AuthController::class, 'logout', []);
+
+Router::add('GET', '/character/form', CharacterController::class, 'form', []);
+Router::add('POST', '/character/analyze', CharacterController::class, 'analyze', []);
 
 Router::run();
